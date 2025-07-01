4dde16041883da8bc7edf20e7d9bd81f3652b72a
tsion1622 <tayu.16.enye@gmail.com>
2025-07-01 16:17:07 +0300

    Add __str__ methods to Book, EBook, and PrintBook to satisfy checker

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,10 +1,10 @@
-# Base Class
+# Base Class - Book
 class Book:
     def __init__(self, title, author):
         self.title = title
         self.author = author
 
-    def get_details(self):
+    def __str__(self):
         return f"Book: {self.title} by {self.author}"
 
 
@@ -14,7 +14,7 @@ class EBook(Book):
         super().__init__(title, author)
         self.file_size = file_size
 
-    def get_details(self):
+    def __str__(self):
         return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
 
 
@@ -24,7 +24,7 @@ class PrintBook(Book):
         super().__init__(title, author)
         self.page_count = page_count
 
-    def get_details(self):
+    def __str__(self):
         return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 
 
@@ -38,4 +38,4 @@ class Library:
 
     def list_books(self):
         for book in self.books:
-            print(book.get_details())
+            print(book)  # Uses __str__ automatically
